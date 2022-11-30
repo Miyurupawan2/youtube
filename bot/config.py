2c9aaf23456b9660f47d54b1d6c756571dbf57a2
Miyurupawan2 <105737539+Miyurupawan2@users.noreply.github.com>
2022-12-01 00:09:10 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -5,8 +5,6 @@ class Config:
 
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-    SESSION_NAME = os.environ.get("SESSION_NAME", ":memory:")
-
     API_ID = int(os.environ.get("API_ID"))
 
     API_HASH = os.environ.get("API_HASH")
